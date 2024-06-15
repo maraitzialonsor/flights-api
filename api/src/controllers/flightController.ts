2c0f29d5e0cc385eeae510bca29bc31d6e159a18
Maraitzi Alonso Rojas <maraitzi.alonso@ibm.com>
2024-06-15 10:30:25 -0600

    Add cache management

--- api/src/controllers/flightController.ts
+++ api/src/controllers/flightController.ts
@@ -1,13 +1,21 @@
 import { Request, Response } from 'express';
 import { FlightModel } from '../models/Flight';
 import { getWeatherData } from '../services/weatherService';
+import NodeCache from 'node-cache';
+import { json } from 'sequelize';
 
+const cache = new NodeCache();
 const flightModel = new FlightModel('./flights.db');
 
 export const getFlightWeather = async (req: Request, res: Response) => {
   const flightNum = req.params.flightNum as string;
 
   try {
+    const cacheFlight = cache.get(flightNum);
+    if(cacheFlight){
+      console.log("Data in cache");
+      return res.json(cacheFlight);
+    }
     const flight = await flightModel.getFlightCoordinates(flightNum);
 
     if (!flight) {
@@ -19,7 +27,7 @@ export const getFlightWeather = async (req: Request, res: Response) => {
     const originWeather = await getWeatherData(origin_latitude, origin_longitude);
     const destinationWeather = await getWeatherData(destination_latitude, destination_longitude);
 
-    res.json({
+    const response = {
       origin: {
         latitude: origin_latitude,
         longitude: origin_longitude,
@@ -30,7 +38,9 @@ export const getFlightWeather = async (req: Request, res: Response) => {
         longitude: destination_longitude,
         weather: destinationWeather
       }
-    });
+    };
+    cache.set(flightNum, response, 300);
+    res.json(response);
   } catch (error) {
     console.error('Error al obtener datos del vuelo:', error);
     res.status(500).json({ error: 'Internal server error' });
